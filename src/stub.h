e6489967141259c61093dea85d871bf71d026c9b
derekdakid <dta858king@gmail.com>
2014-11-16 00:13:26 -0800

    included header file for stub

---
+++ src/stub.h
@@ -0,0 +1,15 @@
+//
+//  stub.h
+//  Crypter
+//
+//  Created by Derek Ta on 11/15/14.
+//
+//
+
+#ifndef __Crypter__stub__
+#define __Crypter__stub__
+
+#include <stdio.h>
+#include <Windows.h>
+
+#endif /* defined(__Crypter__stub__) */
